b87a72a3f4322ec8cb6c8c7d7b40783c426a3803
pitr0theAct <andreyka.sfb@mail.ru>
2024-03-20 16:24:59 +0300

    Add tasks 1-2

--- code/index.php
+++ code/index.php
@@ -1,2 +1,32 @@
 <?php
-echo phpinfo();
+
+
+//1
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+
+// Write your code here:
+$order =& $very_bad_unclear_name;
+$order .= " with sauce";
+
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
+// 2
+
+echo "<br>";
+$intVariable = 1000;
+echo $intVariable. "<br>";
+
+
+$floatVariable = 3.25;
+echo $floatVariable. "<br>";
+
+
+echo (20 - 8). "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month. "<br>";
+
