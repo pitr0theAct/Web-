f3706d44dda9ed1be50aa097ccc804e020c2d49b
pitr0theAct <andreyka.sfb@mail.ru>
2024-03-20 16:58:33 +0300

    Add tasks 13-14

--- code/index.php
+++ code/index.php
@@ -41,3 +41,104 @@ echo $days_per_language . "<br>";
 
 // 12
 echo 8 ** 2 . "<br>";
+
+// 13
+$my_num = 14;
+$answer = $my_num;
+$answer += 2;
+$answer *= 2;
+$answer -= 2;
+$answer /= 2;
+$answer -= $my_num;
+echo $answer . "<br>";
+
+
+// 14
+$a = 10;
+$b = 3;
+echo $a % $b . "<br>";
+
+if ($a % $b == 0) {
+    echo "Делится на $b: " . ($a / $b) . "<br>";
+} else {
+    echo "Делится с остатком: " . ($a % $b) . "<br>";
+
+}
+
+$st = pow(2, 10);
+echo "2 в 10 степени: $st<br>";
+
+$sqrt_245 = sqrt(245);
+echo "Квадратный корень из 245: $sqrt_245<br>";
+
+$numbers = [4, 2, 5, 19, 13, 0, 10];
+$sum_squares = 0;
+foreach ($numbers as $number)
+{
+    $sum_squares += $number ** 2;
+}
+echo "Корень из суммы квадратов: " . sqrt($sum_squares) . "<br>";
+
+echo round(sqrt(379)) . "<br>";
+echo round(sqrt(379), 1) . "<br>";
+echo round(sqrt(379), 2) . "<br>";
+
+echo "Корень из 587: " . ceil(sqrt(587)) . ", " . floor(sqrt(587)) . "<br>";
+
+$numbers = [4, -2, 5, 19, -130, 0, 10];
+echo "Минимальное число: " . min($numbers) . "<br>";
+echo "Максимальное число: " . max($numbers) . "<br>";
+
+echo "Случайное число от 1 до 100: " . rand(1, 100) . "<br>";
+
+$random_numbers = [];
+for ($i = 0; $i < 10; $i++)
+{
+    $random_numbers[] = rand();
+}
+var_dump($random_numbers);
+
+echo "<br>";
+
+$a = 10;
+$b = 7;
+$c = 34;
+$d = -66;
+echo "Модуль разности $a и $b: " . abs($a - $b) . "<br>";
+echo "Модуль разности $c и $d: " . abs($c - $d) . "<br>";
+
+$numbers = [1, 2, -1, -2, 3, -3];
+for ($i = 0; $i < count($numbers); $i++)
+{
+    $numbers[$i] = abs($numbers[$i]);
+}
+var_dump($numbers);
+
+echo "<br>";
+
+$number = 30;
+$divisors = [];
+for ($i = 1; $i <= $number; $i++)
+{
+    if ($number % $i == 0)
+    {
+        $divisors[] = $i;
+    }
+}
+var_dump($divisors);
+
+echo "<br>";
+
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$sum = 0;
+$count = 0;
+foreach ($numbers as $num)
+{
+    $sum += $num;
+    $count++;
+    if ($sum > 10)
+    {
+        break;
+    }
+}
+echo "Количество элементов для суммы больше 10: $count<br>";
